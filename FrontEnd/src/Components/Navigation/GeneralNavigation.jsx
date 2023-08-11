144e3a4417bb3bf12d3f3e1e0606c2303cf48c17
Jakaria44 <mdjakaria442020@gmail.com>
2023-08-11 18:02:40 +0600

    just working fine the nav-sidebar

--- FrontEnd/src/Components/Navigation/GeneralNavigation.jsx
+++ FrontEnd/src/Components/Navigation/GeneralNavigation.jsx
@@ -1,37 +1,8 @@
 import {List, ListItem, ListItemButton, ListItemIcon, ListItemText, ThemeProvider} from "@mui/material";
 import {Home, DensityMedium, Category, FeaturedPlayList} from "@mui/icons-material";
 import {NavLink} from "react-router-dom";
-import {createStyles, makeStyles} from '@mui/styles';
-import {forwardRef} from "react";
 
-
-const MyNavLink = forwardRef((props, ref) => (
-  <NavLink
-    ref={ref}
-    to={props.to}
-    className={({isActive}) => `${props.className} ${isActive ? props.activeClassName : ''}`}
-  >
-    {props.children}
-  </NavLink>
-));
-
-
-const useStyles = makeStyles(({Theme}) =>
-  createStyles({
-    activeLink: {
-      backgroundColor: '#19ABC0',
-      color: '#FFFFFF',
-      borderRadius: 8,
-      '& .MuiSvgIcon-root': {
-        color: '#FFFFFF',
-        stroke: '#FFFFFF',
-        fill: '#19ABC0',
-      },
-    },
-  })
-);
 const GeneralNavigation = () => {
-  const classes = useStyles();
   const navigationsAll = [
     {
       name: "Home",
@@ -55,17 +26,10 @@ const GeneralNavigation = () => {
 
   ]
   return (
-    <ThemeProvider theme={Theme}>
 
     <List>
       {navigationsAll.map((object, index) => (
-        <ListItem component={MyNavLink}
-                  sx={{
-                    color: '#8C8C8C',
-                  }}
-                  activeClassName={classes.activeLink} to={object.page} key={index} disablePadding
-
-        >
+        <ListItem as={NavLink} to={object.page} key={index} disablePadding  className={'active'}  >
           <ListItemButton>
             <ListItemIcon>
               {object.icon}
@@ -75,7 +39,6 @@ const GeneralNavigation = () => {
         </ListItem>
       ))}
     </List>
-    </ThemeProvider>
   )
 }
 
