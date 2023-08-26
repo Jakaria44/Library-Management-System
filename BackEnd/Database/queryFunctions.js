5cdd85fb490ddab2d831e6a58d5653507f1a07e5
Jakaria44 <mdjakaria442020@gmail.com>
2023-08-26 20:16:24 +0600

    merged commits

--- BackEnd/Database/queryFunctions.js
+++ BackEnd/Database/queryFunctions.js
@@ -1,4 +1,4 @@
-import {queryExecute} from './database.js';
+import { queryExecute } from './database.js';
 
 function baseQuery(tableName) {
   return `Select *
@@ -377,35 +377,38 @@ export async function getGenreDB(context) {
   return result.rows;
 }
 
-export async function getAllRatRevOfBookDB(context) {
-  console.log(context);
-  let query = '';
-  if (context.USER_ID) {
-    query += `SELECT *
-              FROM (SELECT U.USER_ID,
-                           (U.FIRST_NAME || ' ' || U.LAST_NAME) AS NAME,
-                           U.IMAGE,
-                           R.RATING,
-                           R.REVIEW,
-                           R.EDIT_DATE` +
-      `\n FROM "USER" U JOIN REVIEW_RATING R ON (U.USER_ID = R.USER_ID)` +
-      `\nWHERE R.ISBN = ${context.ISBN} AND U.USER_ID = ${context.USER_ID}) UNION SELECT * FROM (`;
-  }
-  query +=
-    "SELECT U.USER_ID, (U.FIRST_NAME || ' ' || U.LAST_NAME) AS NAME, U.IMAGE, R.RATING, R.REVIEW, R.EDIT_DATE" +
-    `\nFROM REVIEW_RATING R JOIN "USER" U ON (U.USER_ID = R.USER_ID)` +
-    `\nWHERE R.ISBN = ${context.ISBN}`;
-  if (context.USER_ID) {
-    query += ` AND U.USER_ID <> ${context.USER_ID}` +
-      '\nORDER BY R.EDIT_DATE DESC)';
-  } else {
-    query += '\nORDER BY R.EDIT_DATE DESC';
-  }
-  console.log(query);
-  const result = await queryExecute(query, []);
 
-  return result.rows;
-}
+export async function getAllRatRevOfBookDB(context) {
+    console.log(context);
+    let query = '';
+    let query1 = '';
+    if (context.USER_ID) {
+      query1 = `SELECT U.USER_ID,
+                             (U.FIRST_NAME || ' ' || U.LAST_NAME) AS NAME,
+                             U.IMAGE,
+                             R.RATING,
+                             R.REVIEW,
+                             R.EDIT_DATE` +
+        `\n FROM "USER" U JOIN REVIEW_RATING R ON (U.USER_ID = R.USER_ID)` +
+        `\nWHERE R.ISBN = ${context.ISBN} AND U.USER_ID = ${context.USER_ID}`;
+    }
+    query =
+      "SELECT U.USER_ID, (U.FIRST_NAME || ' ' || U.LAST_NAME) AS NAME, U.IMAGE, R.RATING, R.REVIEW, R.EDIT_DATE" +
+      `\nFROM REVIEW_RATING R JOIN "USER" U ON (U.USER_ID = R.USER_ID)` +
+      `\nWHERE R.ISBN = ${context.ISBN}`;
+    if (context.USER_ID) {
+      query += ` AND U.USER_ID <> ${context.USER_ID}` +
+        '\nORDER BY R.EDIT_DATE DESC';
+    } else {
+      query += '\nORDER BY R.EDIT_DATE DESC';
+    }
+    console.log(query);
+    console.log(query1);
+    const result = await queryExecute(query, []);
+    const result1 = await queryExecute(query1, []);
+    return {allRatRev: result.rows, myRatRev: result1.rows};
+  }
+  
 
 export async function getCompleteBookDB(context) {
   let query = 'SELECT * FROM BOOK B JOIN PUBLISHER P ON (B.PUBLISHER_ID = P.PUBLISHER_ID)';
