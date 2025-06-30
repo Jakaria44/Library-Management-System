e94434d48958f407687e89cc4ca569173cf76cb2
Md. Jakaria Hossain <mdjakaria442020@gmail.com>
2025-06-30 15:37:38 +0600

    Update firebaseConfig.js

--- FrontEnd/src/firebaseConfig.js
+++ FrontEnd/src/firebaseConfig.js
@@ -4,13 +4,13 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyBmh6VJ7RU9KBAAQ6jvJUHatEt80dFJbrM',
-    authDomain: 'library-management-syste-4c8f7.firebaseapp.com',
-    projectId: 'library-management-syste-4c8f7',
-    storageBucket: 'library-management-syste-4c8f7.appspot.com',
-    messagingSenderId: '594700576929',
-    appId: '1:594700576929:web:c77ce643e2df86d1c028e0',
-    measurementId: 'G-GN6LPQ8VKF',
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain:  process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECTID,
+    storageBucket:  process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:  process.env.FIREBASE_MESSAGINGSENDER_ID,
+    appId:  process.env.FIREBASE_APP_ID,
+    measurementId:  process.env.FIREBASE_MESUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
